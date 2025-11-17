523e76d80b51e02baf2c933bef2915fb5aada269
Clarence Baluyot <akingprincess@gmail.com>
2025-11-17 20:03:13 +0800

    try fixing hydration error -6

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
 import { FeaturedEventsSection } from '@/components/FeaturedEventsSection'
 import { PlatinumListService } from '@/services/private/platinumListPrivateService'
+import { Suspense } from 'react'
 
 export default async function HomePage() {
   const eventPlatinumListPrivateService = new PlatinumListService()
-  const [{ data: featuredEvents}] = await Promise.all([
+  const [{ data: featuredEvents }] = await Promise.all([
     eventPlatinumListPrivateService.getEvents(),
   ])
   return (
     <div className="min-h-screen">
-      {/* {<FeaturedEventsSection events={featuredEvents} />} */}
+      <Suspense fallback={<div>Loading featured events...</div>}>
+        {<FeaturedEventsSection events={featuredEvents} />}
+      </Suspense>
     </div>
   )
 }
